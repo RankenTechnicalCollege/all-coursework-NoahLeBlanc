b003b5637b62edd2cd126c0b29fa628d851995d5
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-08-30 12:53:55 -0500

    Tweaked the Output vars to make sure there would be no conflicts.

--- labs/lab01_1/ex2/tempConverter.mjs
+++ labs/lab01_1/ex2/tempConverter.mjs
@@ -5,25 +5,20 @@ const rl = readline.createInterface({ input, output });
 
 let tempt = parseFloat(await rl.question('Please Input the Temp you\'d like to convert:'));
 let celsiusOrFahrenheit = await rl.question('Convert to [C]elsius Or [F]ahrenheit?: ');
-
+celsiusOrFahrenheit = celsiusOrFahrenheit.toString().toLowerCase();
 
 if(isNaN(tempt) || (tempt <= 0)){
     console.log("Please enter a valid number for temperature")
 }
-else if ((celsiusOrFahrenheit.toString().toLowerCase()) == "c" || (celsiusOrFahrenheit.toString().toLowerCase()) == "celsius") {
-    const output = (tempt - 32) * (5 / 9);
-    console.log(`You're temp ${tempt} Fahrenheit is ${output.toFixed(2)} Celsius`);
+else if ((celsiusOrFahrenheit) == "c" || (celsiusOrFahrenheit) == "celsius") {
+    const outputCelsius = (tempt - 32) * (5 / 9);
+    console.log(`You're temp ${tempt} Fahrenheit is ${outputCelsius.toFixed(2)} Celsius`);
 }
-else if ((celsiusOrFahrenheit.toString().toLowerCase()) == "f" || (celsiusOrFahrenheit.toString().toLowerCase()) == "fahrenheit") {
-    const output = (tempt * (9 / 5)) + 32;
-    console.log(`You're temp ${tempt} Celsius is ${output.toFixed(2)} Fahrenheit`);
+else if ((celsiusOrFahrenheit) == "f" || (celsiusOrFahrenheit) == "fahrenheit") {
+    const outputFahrenheit = (tempt * (9 / 5)) + 32;
+    console.log(`You're temp ${tempt} Celsius is ${outputFahrenheit.toFixed(2)} Fahrenheit`);
 }
 else {
     console.log("Error please input [C]elsius Or [F]ahrenheit.");
 }
-
-
-
-
-
 rl.close();
