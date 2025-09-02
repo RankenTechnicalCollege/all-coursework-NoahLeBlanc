f22c541dbb0b810043f89597cb5ec7a50ed2fca0
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-02 15:08:48 -0500

    Got the website working on localhost:3000

---
+++ FinalProject/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('frontend/dist'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+    console.log(`server is running on port http://localhost:${port}`);
+})
+
+app.get('/api', (req, res) =>{
+    res.send('Hello World from the back end route')
+});
