9657f814ab4356806bb76a2d973448d73e98814c
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-02 16:20:31 -0500

    First day of work done on the lab01-02

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config(); //This sets up process.env.PORT
+import debug from 'debug';
+
+const debugServer = debug('app:Server');//Like a fancy console.log
 
 const app = express();
 
@@ -9,8 +14,8 @@ app.use(express.static('frontend/dist'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
-    console.log(`server is running on port http://localhost:${port}`);
-})
+    debugServer(`server is now running on port http://localhost:${port}`);
+});
 
 app.get('/api', (req, res) =>{
     res.send('Hello World from the back end route')
