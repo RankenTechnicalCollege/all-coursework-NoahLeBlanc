88b8725cbd1bad7b908361908a9e95d56e622d5a
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-03 16:21:41 -0500

    Added bug.js and user.js Updated index.js.

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -3,19 +3,17 @@ import dotenv from 'dotenv'
 dotenv.config(); //This sets up process.env.PORT
 import debug from 'debug';
 const debugServer = debug('app:Server');//Like a fancy console.log
+import { userRouter } from './routes/api/user.js';//this is a route
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('frontend/dist'));
-
+app.use('/api/user', userRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
     debugServer(`server is now running on port http://localhost:${port}`);
 });
 
-app.get('/api', (req, res) =>{
-    res.send('Hello World from the back end route')
-});
