ed259d0ab0727722747ac819256112cb79a6c360
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-05 15:42:14 -0500

    Added some basic UI to the website.

--- FinalProject/frontend/src/App.jsx
+++ FinalProject/frontend/src/App.jsx
@@ -1,23 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
-function App() {
 
+
+function App() {
   return (
     <>
-      <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-            <span class="fs-4">Simple header</span>
-          </a>
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-          </ul>
-        </header>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <a className="navbar-brand d-flex align-items-center gap-2" href="#">
+        <i className="bi bi-code-slash"></i>
+        <span className="h4 mb-0">Bug Tracker</span>
+      </a>
+      <ul className="navbar-nav ms-3">
+        <li className="nav-item">
+          <a className="nav-link text-light" href="#">Users</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-light" href="#">Bugs</a>
+        </li>
+      </ul>
+    </nav>  
+
+    <main className='d-flex justify-content-evenly'> 
+      <div className='d-flex justfiy-content-center'>
+        <h1>Users</h1>  
       </div>
+
+      <div className='d-flex justfiy-content-center'>
+        <h1>Bugs</h1>  
+      </div>
+    </main>
+
+    <footer className="footer bg-dark text-light">
+      <div className="container">
+        <span className="">@2025 Niah LeBlanc</span>
+      </div>
+    </footer>
     </>
   )
 }
