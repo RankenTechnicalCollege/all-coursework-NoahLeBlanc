dc8b6ac629a68f5ef525ceec82786019513446a6
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-10 14:09:42 -0500

    Reformated my folders to be more clean

---
+++ Labs/Lab01/01/ex1/mpg.mjs
@@ -0,0 +1,18 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const milesDriven = parseFloat(await rl.question('Miles Driven: '));
+const gallonsUsed = parseFloat(await rl.question('Gallons used: '));
+
+if(isNaN(milesDriven) || isNaN(gallonsUsed)){
+  console.log("Error: Please Input a number")
+}
+else if(milesDriven <= 0 || gallonsUsed <= 0){
+  console.log("Error: Value's must be greater than 0")
+}
+else{
+  console.log(`Your MPG is ${(milesDriven / gallonsUsed).toFixed(2)}`)
+}
+rl.close();
---
+++ Labs/Lab01/01/ex4/multiplicationTable.mjs
@@ -0,0 +1,24 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { isStringObject } from 'node:util/types';
+
+//variables 
+const rl = readline.createInterface({ input, output });
+const tableSize = parseInt(await rl.question("How big do you want your table to be (1-12)?:"));
+
+let a = 1;
+
+while (!(a > tableSize)) {
+    let b = 1;
+    let row = [];
+
+    while (!(b > tableSize)) {
+        row.push((b * a).toFixed().padStart(4, " ")); // pad to align columns
+        b++;
+    }
+
+    console.log(row.join("")); // print one row at a time
+    a++;
+}
+
+rl.close();
