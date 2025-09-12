af8ad8adf7cd985747e54281c4e372759de77584
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-12 16:35:16 -0500

    I finished the routes!

--- HandsOnTests/HOT1/server.js
+++ HandsOnTests/HOT1/server.js
@@ -4,23 +4,26 @@ dotenv.config(); //This sets up process.env.PORT
 import debug from 'debug';
 const debugServer = debug('app:Server');//Like a fancy console.log
 
+//---------------------------- Import Routes -----------------------------
 import { incomeRouter } from './routes/api/income-tax/calc.js';
+import { interestRouter } from './routes/api/interest/calc.js';
 import { MPGRouter } from './routes/api/mpg/calc.js';
 import { tempRouter } from './routes/api/temperature/convert.js';
 
+//-------------------------- Express Handling ---------------------------
 const app = express();
-
-app.use(express.urlencoded({extended: true})); //no reqs work without this
-app.use(express.json());//needed for req.body
-
+app.use(express.urlencoded({ extended: true })); // no reqs work without this
+app.use(express.json()); // needed for req.body
 app.use(express.static('frontend/dist'));
+
+//------------------------------ Routes --------------------------------
 app.use('/api/mpg/', MPGRouter);
 app.use('/api/temp/', tempRouter);
 app.use('/api/incomeTax/', incomeRouter);
+app.use('/api/interest/', interestRouter);
 
+//--------------------------- Port Handling -----------------------------
 const port = process.env.PORT || 3000;
-
-app.listen(port, () =>{
+app.listen(port, () => {
     debugServer(`server is now running on port http://localhost:${port}`);
 });
-
