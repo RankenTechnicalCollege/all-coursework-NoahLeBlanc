2f1855231f23db417d97fb691399ec3816a06c84
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-09-16 13:06:20 -0500

    Added Everything

--- HandsOnTests/HOT1/frontend/src/App.jsx
+++ HandsOnTests/HOT1/frontend/src/App.jsx
@@ -4,7 +4,38 @@ function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <header>
+        <h1>Hot1</h1>
+      </header>
+      <main>
+        
+        <div>
+          <input type="number" name="milesDriven" id="milesDriven" />
+          <input type="number" name="gallonsUsed" id="gallonsUsed" />
+        </div>
+
+        <div>
+          <input type="radio" name="single" id="single" />
+          <input type="radio" name="married" id="married" />
+          <input type="number" name="taxableIncome" id="taxableIncome" />
+        </div>
+
+        <div>
+          <input type="radio" name="CtoF" id="CtoF" />
+          <input type="radio" name="FtoC" id="FtoC" />
+          <input type="number" name="temp" id="temp" />
+        </div>
+
+        <div>
+          <input type="number" name="principle" id="principle" />
+          <input type="number" name="interestRate" id="interestRate" />
+          <input type="number" name="years" id="years" />
+        </div>
+
+      </main>
+      <footer>
+        <h3>Ⓒ Niah LeBlanc</h3>
+      </footer>
     </>
   )
 }
