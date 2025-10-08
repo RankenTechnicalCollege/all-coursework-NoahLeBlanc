c80b54e2a9ef9b38a8a53204cc235c12254e4cc6
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-10-08 15:57:30 -0500

    ( chore ): Setup Catch routes for ID error handling

--- FinalProject/routes/api/test.js
+++ FinalProject/routes/api/test.js
@@ -3,46 +3,81 @@ import { newId, getCollection } from '../../database.js';
 import debug from 'debug';
 import { ObjectId } from 'mongodb';
 
-//To Do
+const router = express.Router();
+const debugTests = debug(`app:BugRouter`)
+
+router.use(express.urlencoded ({extended: false}))
+router.use(express.json());
+
+let bugCollection = await getCollection('bugs');
+
 // =====================================================================================
 //                                       [ GET REQUESTS ] 
 // =====================================================================================
 // ===========================================
-//    [ GET ALL TESTS FOR A SPECIFIC BUG ]
+//    [ GET ALL TESTS FOR A SPECIFIC BUG ]   |
 // ===========================================
 //GET /api/bugs/:bugId/tests(10pts)
 router.get('/:bugId/tests', async (req, res) => {
-try{
-
-}
-catch(err){
-
-}});
+    try{
+        debugTests(`DEBUG TEST: fetching tests for bugId`);
+        const { bugId } = req.params;
+        // Validate ID before using it
+        validateID(bugId);
+        debugTests('Made it past');
+    }
+    catch(err){
+        console.error(err);
+        if(err.status){
+            res.status(err.status).json({error: err.message})
+        }
+        else{
+            res.status(500).json({ error: 'Server error' });
+        }
+    }});
 // ===========================================
-//  [ GET SPECIFIC TEST FOR A SPECIFIC BUG ]
+//  [ GET SPECIFIC TEST FOR A SPECIFIC BUG ] |
 // ===========================================
 //GET /api/bugs/:bugId/tests/:testId(10pts)
 router.get('/:bugId/tests/:testId', async (req, res) => {
-try{
+    try{
+        debugTests(`DEBUG TEST: fetching specific test for bugId`);
+        const {bugId, testId} = req.params;
+        validateID(bugId);validateID(testId);
 
-}
-catch(err){
-
-}});
+    }
+    catch(err){
+        console.error(err);
+        if(err.status){
+            res.status(err.status).json({error: err.message})
+        }
+        else{
+            res.status(500).json({ error: 'Server error' });
+        }
+    }});
 // =====================================================================================
 //                                       [ POST REQUESTS ] 
 // =====================================================================================
 // ===========================================
-//   [ CREATE NEW TEST FOR A SPECIFIC BUG]
+//   [ CREATE NEW TEST FOR A SPECIFIC BUG]   |
 // ===========================================
 //POST /api/bugs/:bugId/tests(10pts)
 router.post('/:bugId/tests', async (req, res) => {
-try{
+    try{
+        debugTests(`DEBUG TEST: adding test to bugId`);
+        const {bugId, testId} = req.params;
+        validateID(bugId);validateID(testId);
 
-}
-catch(err){
-
-}});
+    }
+    catch(err){
+        console.error(err);
+        if(err.status){
+            res.status(err.status).json({error: err.message})
+        }
+        else{
+            res.status(500).json({ error: 'Server error' });
+        }
+    }});
 // =====================================================================================
 //                                       [ PATCH REQUESTS ] 
 // =====================================================================================
@@ -51,12 +86,21 @@ catch(err){
 // ===========================================
 //PATCH /api/bugs/:bugId/tests/:testId
 router.patch('/:bugId/tests/:testId', async (req, res) => {
-try{
+    try{
+        debugTests(`DEBUG TEST: updating test to bugId`);
+        const {bugId, testId} = req.params;
+        validateID(bugId);validateID(testId);
 
-}
-catch(err){
-
-}});
+    }
+    catch(err){
+        console.error(err);
+        if(err.status){
+            res.status(err.status).json({error: err.message})
+        }
+        else{
+            res.status(500).json({ error: 'Server error' });
+        }
+    }});
 // =====================================================================================
 //                                       [ DELETE REQUESTS ] 
 // =====================================================================================
@@ -65,12 +109,35 @@ catch(err){
 // ===========================================
 //DELETE /api/bugs/:bugId/tests/:testId
 router.delete('/:bugId/tests/:testId', async (req, res) => {
-try{
+    try{
+        debugTests(`DEBUG TEST: delete testID on bugId`);
+        const {bugId, testId} = req.params;
+        validateID(bugId);validateID(testId);
 
-}
-catch(err){
-
-}});
+    }
+    catch(err){
+        console.error(err);
+        if(err.status){
+            res.status(err.status).json({error: err.message})
+        }
+        else{
+            res.status(500).json({ error: 'Server error' });
+        }
+    }});
 
 // =======================================[ EXPORT ROUTER ]=============================
 export {router as testRouter};
+
+
+
+// ===========================================
+//            [ HELPER FUNCTION ]
+// ==========================================
+function validateID(i) {
+  if (!ObjectId.isValid(i)) {
+    const err =  new Error(`${i} is not a valid ObjectId.`);
+    err.status == 400;
+    throw err;
+  }
+  debugTests(`${i} Passed ID validation`);
+}
