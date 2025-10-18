56598a25736b09a884803d850fc5b69b568e9875
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-10-17 22:47:14 -0500

    (feat): add Validate queary

--- FinalProject/middleware/validation.js
+++ FinalProject/middleware/validation.js
@@ -36,3 +36,22 @@ export const validBody = (schema) => {
     next();
   };
 };
+//|==================================================|
+//|[----------------[-VALIDATE-QUERY-]---------------|
+//|==================================================|
+export const validQuery = (schema) => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req.query, {
+      abortEarly: false,
+      stripUnknown: false,
+    });
+    if (error) {
+      return res.status(400).json({
+        error: 'Validation failed',
+        details: error.details.map(detail => detail.message),
+      });
+    };
+    req.validatedQuery = value; // ✅ Store validated query in a custom field
+    next();
+  };
+};
