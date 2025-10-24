cdab4b76c5f877bd69d2651e7976fd650df1b3a1
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-10-24 15:34:50 -0500

    (feat): implement auth from better-auth

--- FinalProject/auth.js
+++ FinalProject/auth.js
@@ -1,31 +1,41 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
 import { getClient, getDatabase } from "./database.js";
 
 const client = await getClient();
 const db = await getDatabase();
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:8080",
-  trustedOrigins: ["http://localhost:5000","http://localhost:5050", "http://localhost:3000", "http://localhost:8080"],
-  database: mongodbAdapter(db, { client }),
-  emailAndPassword: {
-    enabled: true,
-  },
-  session:{
-    cookieCache: true,
-    maxAge: 60 * 60,
-  },
-  user:{
-    additionalFields:{
-      role:{
-        type: "object", 
-        required: false
-      },
-      profile:{
-        type: "object", 
-        required: false
-      }
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000",
+    trustedOrigins: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "https://lawnconnect-service-294349793000.us-central1.run.app"
+    ],
+
+   database: mongodbAdapter(db, {
+        client
+    }),
+     emailAndPassword: { 
+        enabled: true, 
+    },
+     session:{
+        cookieCache:true,
+        maxAge: 60 * 60 * 1000 // 1 hour
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: "object", // MongoDB stores arrays as objects
+                required: false,
+            },
+            profile: {
+                type: "object",
+                required: false,
+            }
+        }
     }
-  }
+
 });
