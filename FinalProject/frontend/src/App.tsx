def74952fe30015987c5bb722846eef4098bf405
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-11-23 18:54:13 -0600

    (feat) userEdit, bugEdit

--- FinalProject/frontend/src/App.tsx
+++ FinalProject/frontend/src/App.tsx
@@ -7,6 +7,8 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import BugList from "./components/pages/bugList";
 import { UserList } from "./components/pages/userList";
+import BugEdit from "./components/pages/bugEdit";
+import UserEdit from "./components/pages/userEdit";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +22,8 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/BugList" element={<BugList />} />
           <Route path="/UserList" element={<UserList />} />
+          <Route path="/BugEdit" element={<BugEdit/>} />
+          <Route path="/UserEdit" element={<UserEdit/>} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
