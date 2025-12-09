20d0d11fd511f760776a74bc78cc5edd019d83fc
Niah LeBlanc <noah_leblanc@insideranken.org>
2025-12-09 15:07:16 -0600

    (feat): init shadcn

--- FinalProject/frontend/vite.config.ts
+++ FinalProject/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -12,4 +13,11 @@ export default defineConfig({
       },
     }),
   ],
+  //shadcn
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, "./src"),
+    }
+
+  },
 })
