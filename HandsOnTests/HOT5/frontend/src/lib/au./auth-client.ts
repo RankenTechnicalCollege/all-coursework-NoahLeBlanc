6c82915f3c7716b89108c30b8b2cdcc54bd41a22
Niah LeBlanc <noah_leblanc@insideranken.org>
2026-02-18 13:28:11 -0600

    Delete HandsOnTests/HOT5 directory

--- HandsOnTests/HOT5/frontend/src/lib/au./auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/auth`
-    : "https://localhost:2023/api/auth"
-})
